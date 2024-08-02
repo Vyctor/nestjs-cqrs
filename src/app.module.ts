9170a14b846abb53228b15b844a52ae96643b44f
Vyctor <dev.vyctor@gmail.com>
2024-08-02 12:50:58 -0300

    modelagem relacional com typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContactInfo } from './entities/contact-info.entity';
+import { Employee } from './entities/employee.entity';
+import { Meeting } from './entities/meeting.entity';
+import { Task } from './entities/task.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+    TypeOrmModule.forFeature([Employee, Task, ContactInfo, Meeting]),
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, unlinkSync } from 'fs';
+import { AppService } from './app.service';
 
 async function bootstrap() {
+  deleteDbFile('db.sqlite');
   const app = await NestFactory.create(AppModule);
+
+  const appService = app.get(AppService);
+  await appService.seed();
+
   await app.listen(3000);
+}
+
+function deleteDbFile(dbFileName: string) {
+  if (existsSync(dbFileName)) {
+    unlinkSync(dbFileName);
+  }
 }
 bootstrap();
